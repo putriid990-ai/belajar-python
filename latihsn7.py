0d4936ae428699cbc0c71a21f041f37a59e91e1c
putriid990-ai <putriid990@gmail.com>
2026-06-28 08:14:13 +0700

    Create latihsn7.py

---
+++ latihsn7.py
@@ -0,0 +1,19 @@
+# Daftar belanja
+belanja = []
+
+# Tambah 5 item belanja
+belanja.append("Beras")
+belanja.append("Telur")
+belanja.append("Minyak")
+belanja.append("Gula")
+belanja.append("Garam")
+
+print("=== DAFTAR BELANJA ===")
+print("Total item:", len(belanja))
+print("")
+
+for item in belanja:
+    print("-", item)
+
+print("")
+print("Belanja selesai!")
