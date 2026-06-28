c1d59c48200ff96f230675bec1ffaf761daf7270
putriid990-ai <putriid990@gmail.com>
2026-06-28 08:21:00 +0700

    Create latihan8.py

---
+++ latihan8.py
@@ -0,0 +1,14 @@
+biodata = {
+    "nama": "Putri",
+    "umur": 20,
+    "kota": "Lampung",
+    "jurusan": "Ilmu Komputer",
+    "hobi": "jalan-jalan"
+}
+
+print("=== BIODATA ===")
+print("Nama   :", biodata["nama"])
+print("Umur   :", biodata["umur"], "tahun")
+print("Kota   :", biodata["kota"])
+print("Jurusan:", biodata["jurusan"])
+print("Hobi   :", biodata["hobi"])
